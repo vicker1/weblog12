cee03583bee66285256ed4cc1fb905efe4e13de5
unknown <vicker1>
2019-09-30 07:18:31 -0700

    creating migration,model,controller,factories

---
+++ app/Http/Controllers/ReplyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\model\reply;
+use Illuminate\Http\Request;
+
+class ReplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\model\reply  $reply
+     * @return \Illuminate\Http\Response
+     */
+    public function show(reply $reply)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\model\reply  $reply
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(reply $reply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\model\reply  $reply
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, reply $reply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\model\reply  $reply
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(reply $reply)
+    {
+        //
+    }
+}
